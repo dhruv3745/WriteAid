5abf40fda676e7f4288d2df34136c789e89086b6
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2023-01-21 15:47:44 -0500

    /capture to get moment in frame to bytes

--- WriteAidReact/App.js
+++ WriteAidReact/App.js
@@ -2,8 +2,24 @@ import * as React from 'react';
 import MainContainer from './navigation/MainContainer';
 
 function App() {
+
+  setInterval(stream_data, 33);
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+  });
+
   return(
     <MainContainer/>
   );
+}
+
+async function stream_data() {
+  console.log('ER');
+  fetch("/capture").then(
+    res => res.json()
+  )
 }
 export default App;
