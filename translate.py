30b3aa9b5d7a1204a482367d4403d382ffe64bb3
AadiJangid <90218443+AadiJangid@users.noreply.github.com>
2023-01-22 00:35:52 -0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,9 +1,11 @@
 #pip install googletrans==4.0.0rc1
-
 import googletrans
 
-textIn = "明天你要做什么"
-translator = googletrans.Translator()
-textOut = translator.translate(textIn, dest="en").text
 
-print(textOut)
+def translation(textIn):
+    translator = googletrans.Translator()
+    return translator.translate(textIn, dest="en").text
+
+
+
+print(translation("明天你要做什么"))
