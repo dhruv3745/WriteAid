565f04028e55206e68d43b61ee78a91ba91966e9
AadiJangid <90218443+AadiJangid@users.noreply.github.com>
2023-01-22 00:33:49 -0500

    Update textToImage.py

--- textToImage.py
+++ textToImage.py
@@ -1,58 +1,61 @@
 import cv2
 import numpy as np
 
-textIn = "Hello how are you doing today"
-textIn2 = "Hello how are you doing today. This is a new sentence to test if it wraps or not"
-img = np.ones((296, 607, 3), dtype = np.uint8)
-img = 255*img
+text = "Hello how are you doing today. This is a new sentence to test if it wraps or not"
 
-charList = []
-for character in textIn:
-    charList.append(character)
+def textToImage(textIn):
+    img = np.ones((296, 607, 3), dtype=np.uint8)
+    img = 255 * img
 
-print(charList)
+    charList = []
+    for character in textIn:
+        charList.append(character)
 
-if (len(charList) >= 26):
-    # TODO add
-    print("Hello")
-    counter = -1
-    index = 0
-    quitLoop = False
-    while True:
-        tempList = []
-        counter += 1
-        for character in charList[index:]:
-            quitLoop = False
-            tempList.append(character)
-            if character == " ":
-                nextLength = 0
-                for letter in charList[index + 1:]:
-                    nextLength += 1
-                    if letter == " ":
-                        break
-                if len(tempList) + nextLength > 26:
-                    quitLoop = True
+    tempBool = False
 
-            index += 1
-            if len(tempList) == 26 or index == len(charList) - 1 or quitLoop:
+    if (len(charList) >= 26):
+        # TODO add
+        print("Hello")
+        counter = 0
+        index = 0
+        quitLoop = False
+        while True:
+            tempList = []
+            for character in charList[index:]:
+                quitLoop = False
+                tempList.append(character)
+                if character == " ":
+                    nextLength = 0
+                    for letter in charList[index + 1:]:
+                        nextLength += 1
+                        if letter == " ":
+                            break
+                    if len(tempList) + nextLength > 26:
+                        quitLoop = True
+
+                index += 1
+                if len(tempList) == 26 or index == len(charList) or quitLoop:
+                    break
+                quitLoop = False
+
+            if tempBool:
                 break
-            quitLoop = False
-
-        if index == len(charList) - 1:
-            break
-        resultText = "".join(tempList)
-        cv2.putText(img, resultText, (25, 35 + (counter * 50)), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), 5, 2)
-
-
-else:
-    diff = (26 - len(charList))
-    cv2.putText(img, textIn, (25, int(75 - (diff * 0.5))), cv2.FONT_HERSHEY_SIMPLEX, 1.2 + (diff * 0.08), (0, 0, 0), 5, 2)
+            if index == len(charList):
+                tempBool = True
+            resultText = "".join(tempList)
+            cv2.putText(img, resultText, (25, 35 + (counter * 50)), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), 5, 2)
+            counter += 1
 
 
+    else:
+        diff = (26 - len(charList))
+        cv2.putText(img, textIn, (25, int(75 - (diff * 0.5))), cv2.FONT_HERSHEY_SIMPLEX, 1.2 + (diff * 0.08), (0, 0, 0),
+                    5, 2)
 
-#cv2.putText(img, textIn, (25,35), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,0), 2, 2)
+    # cv2.putText(img, textIn, (25,35), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,0), 2, 2)
 
-cv2.imshow("Image",img)
-cv2.imwrite("resultImage.jpg", img)
-cv2.waitKey(0)
+    cv2.imshow("Image", img)
+    cv2.imwrite("resultImage.jpg", img)
+    cv2.waitKey(0)
 
+textToImage(text)
